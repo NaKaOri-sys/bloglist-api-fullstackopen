5a345b20e2353b45c45c552fc2c22498663078ad
Nahuel Nievas <nahuelnievas30@gmail.com>
2025-08-28 12:26:02 -0300

    feat: done exercise 4.22

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,87 +1,79 @@
-const blogsRouter = require('express').Router()
-const jwt = require('jsonwebtoken')
-const logger = require('../utils/logger')
-const Blog = require('../models/blog')
-const User = require('../models/user')
+const blogsRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
+const logger = require("../utils/logger");
+const Blog = require("../models/blog");
+const User = require("../models/user");
+const middleware = require("../utils/middleware");
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.json(blogs);
+});
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
-  response.json(blogs)
-})
-
-blogsRouter.get('/:id', async (request, response) => {
-  const id = request.params.id
-  const blog = await Blog.findById(id).populate('user')
+blogsRouter.get("/:id", async (request, response) => {
+  const id = request.params.id;
+  const blog = await Blog.findById(id).populate("user");
   if (!blog) {
-    response.status(404).json({ error: 'No blog found with the given ID.' })
-    return
+    response.status(404).json({ error: "No blog found with the given ID." });
+    return;
   }
-  response.json(blog)
-})
+  response.json(blog);
+});
 
-blogsRouter.post('/', async (request, response) => {
-  const { title, author, url, likes } = request.body  
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
-
+blogsRouter.post("/", middleware.userExtractor, async (request, response) => {
+  const { title, author, url, likes } = request.body;
+  const user = request.user;
   if (!user) {
-    response.status(404).json({ error: 'No user found with the fiven ID.' })
-    return
+    return response.status(401).json({ error: "Token missing or invalid" });
   }
 
   const blog = new Blog({
-    title, author, url, likes, user: user._id
-  })
-  const result = await blog.save()
-  user.blogs = user.blogs.concat(result._id)
-  await user.save()
+    title,
+    author,
+    url,
+    likes,
+    user: user._id,
+  });
+  const result = await blog.save();
+  user.blogs = user.blogs.concat(result._id);
+  await user.save();
 
-  response.status(201).json(result)
-})
+  response.status(201).json(result);
+});
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete("/:id", middleware.userExtractor, async (request, response) => {
   const id = request.params.id
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
+  const user = request.user
+  if (!user) {
+    return response.status(401).json({ error: "Token missing or invalid" });
   }
-  const blog = await Blog.findById(id)
-  if (blog.user.toString() !== decodedToken.id.toString()) {
-    response.status(401).json({ error: 'Only user has been create blog can delete it.' })
-    return
+  const blog = await Blog.findById(id);
+  if (blog.user.toString() !== user.id.toString()) {
+    if (!blog) {
+      response.status(404).json({ error: "No blog found with the given ID." });
+      return;
+    }
+    response.status(401).json({ error: "Only user has been create blog can delete it." });
+    return;
   }
-  if (!blog) {
-    response.status(404).json({ error: 'No blog found with the given ID.' })
-    return
-  }
-  await Blog.findByIdAndDelete(id)
-  response.status(204).end()
-})
+  await Blog.findByIdAndDelete(id);
+  response.status(204).end();
+}
+);
 
-blogsRouter.put('/:id', async (request, response) => {
-  const id = request.params.id
-  const { likes } = request.body
-  if (typeof likes !== 'number' || likes < 0) {
-    response.status(400).json({ error: 'Likes must be non-negative number.' })
-    return
+blogsRouter.put("/:id", async (request, response) => {
+  const id = request.params.id;
+  const { likes } = request.body;
+  if (typeof likes !== "number" || likes < 0) {
+    response.status(400).json({ error: "Likes must be non-negative number." });
+    return;
   }
-  const res = await Blog.findByIdAndUpdate(id, { likes }, { new: true })
+  const res = await Blog.findByIdAndUpdate(id, { likes }, { new: true });
   if (!res) {
-    response.status(404).json({ error: 'No blog found with the given ID.' })
-    return
+    response.status(404).json({ error: "No blog found with the given ID." });
+    return;
   }
-  response.json(res)
-})
+  response.json(res);
+});
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
